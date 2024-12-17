4eb3fd1c3169e4961bff26daf48058230a4cde30
André Duarte <andrexduarte@gmail.com>
2024-12-17 10:41:06 -0300

    delete

--- plugins/format-date.ts
+++
@@ -1,9 +0,0 @@
-import { defineNuxtPlugin } from '#app'
-
-export default defineNuxtPlugin(() => {
-    return {
-        provide: {
-            formatDate: (dateStr) => (dateStr ? Intl.DateTimeFormat('us-EN', { dateStyle: 'medium' }).format(new Date(dateStr)) : undefined)
-        }
-    };
-});
