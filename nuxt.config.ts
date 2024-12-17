276683010fd2245a8b65728356a321f44d2644ce
André Duarte <andrexduarte@gmail.com>
2024-12-17 11:11:04 -0300

    update nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,7 +43,8 @@ export default defineNuxtConfig({
       ]
     },
 
-    baseURL: '/adx/' // baseURL: '/<repository>/'
+    baseURL: '/adx/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets',
   },
 
   vite: {
